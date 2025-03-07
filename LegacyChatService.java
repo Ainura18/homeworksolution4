016946d2c98dcb439737f3da1e74b4c1ace838c9
Ainura18 <157133099+Ainura18@users.noreply.github.com>
2025-03-07 19:58:49 +0500

    Create LegacyChatService.java

---
+++ LegacyChatService.java
@@ -0,0 +1,7 @@
+package chat;
+
+public class LegacyChatService {
+    public void legacySendMessage(String oldMessageFormat) {
+        System.out.println("Legacy Chat: " + oldMessageFormat);
+    }
+}
